cf8c4430c8ee403f8fe18e3d3973f9d979ffe708
srujangopu <srujanjack@gmail.com>
2016-02-11 19:57:55 -0600

    checkout page

--- app/checkout/checkout.ts
+++ app/checkout/checkout.ts
@@ -1,3 +1,49 @@
 /**
  * Created by srujangopu on 2/9/16.
  */
+
+import {bootstrap} from 'angular2/bootstrap'
+import {Component, Inject} from 'angular2/core';
+import {
+    FormBuilder,
+    Validators,
+    Control,
+    ControlGroup,
+    FORM_DIRECTIVES,
+    CORE_DIRECTIVES
+} from 'angular2/common';
+
+/*// An example of typical model
+class Address {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+}*/
+
+@Component({
+    selector: 'checkout',
+    templateUrl: 'app/checkout/checkout.html',
+    styleUrls: ['app/checkout/checkout.css'],
+    directives: [FORM_DIRECTIVES]
+})
+
+export class Checkout {
+    form: ControlGroup;
+
+    firstName: Control;
+
+    constructor(@Inject(FormBuilder) private builder: FormBuilder) {
+
+        this.firstName = new Control("", Validators.compose([Validators.required]));
+
+        this.form = builder.group({
+            firstName:  this.firstName
+        });
+    }
+
+    submitData(){
+        console.log(JSON.stringify(this.form.value))
+    }
+}
+
--- app/home/home.ts
+++ app/home/home.ts
@@ -4,13 +4,14 @@
 import {Component} from 'angular2/core';
 import {Search} from '../search/search.ts'
 import {ViewCart} from '../viewCart/viewCart.ts'
+import {Checkout} from '../checkout/checkout.ts'
 import {Header} from '../header/header.ts'
 
 @Component({
     selector: 'home',
     templateUrl: 'app/home/home.html',
     styleUrls: ['app/home/home.css'],
-    directives: [Search, ViewCart, Header]
+    directives: [Search, ViewCart,Checkout, Header]
 })
 export class Home {
 
